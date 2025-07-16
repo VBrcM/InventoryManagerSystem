908cfc3a721a4ef307aed488def9d984b32e3a28
Ziel <alhpajarillo@mymail.mapua.edu.ph>
2025-07-16 20:08:36 +0800

    database connection

---
+++ src/DB/JDBC.java
@@ -0,0 +1,22 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+    public static Connection connect() {
+        String url = "jdbc:mysql://127.0.0.1:3306/hardware_inventory_management";
+        String user = "school";
+        String password = "Jan2024108219";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected successfully!");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            return null;
+        }
+    }
+}
